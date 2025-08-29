697b02ebd721854eb80b743f5f6aee7004438272
armin9119 <armin91198452@gmail.com>
2025-08-29 23:47:54 +0330

    dey 22: Add app description, image, and links

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+# streamlit run app.py
+# http://localhost:8501
+
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
@@ -21,9 +24,20 @@ Below you can find some of the apps i have built in python. Feel free to contact
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col,col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pd.read_csv('data.csv', sep=';')
 with col3:
-    for index, row in df.iterrows():
+    for index, row in df[ :10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f'[Source Code]({row['url']})')
+
+
+with col4:
+    for index, row in df[10:20].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f'[Source Code]({row['url']})')
