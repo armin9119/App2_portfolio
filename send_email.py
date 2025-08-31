544154f957e302bf1d1bf3c646a4b06976023aa8
armin9119 <armin91198452@gmail.com>
2025-08-31 19:29:48 +0330

    dey 23: Send email using the from

--- send_email.py
+++ send_email.py
@@ -1,26 +1,18 @@
 import smtplib
 import ssl
 
-host = 'smtp.gmail.com'
-port = 465
 
-username = 'armincook7@gmail.com'
-password = 'epfl zcaz lrwx fyra'
+def send_email(receiver, message):
+    host = 'smtp.gmail.com'
+    port = 465
+    username = 'armincook7@gmail.com'
+    password = 'epfl zcaz lrwx fyra'
+    context = ssl.create_default_context()
 
-receiver = 'armincook7@gmail.com'
-
-context = ssl.create_default_context()
-
-message = """\
-Subject: Hi
-How are you?
-Bye!
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
 
-# Add navigation menu and
-# Contact Us page and send email
+
+
